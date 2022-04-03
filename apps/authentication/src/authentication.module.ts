480f2c0d124a7a4fc931c55bada45a592ee691be
IbrohimIsroilov <tursunali777@mail.ru>
2022-04-03 14:07:51 +0900

    fix: set up

--- apps/authentication/src/authentication.module.ts
+++ apps/authentication/src/authentication.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
-import { LOEYBConfigModule } from 'configs/loeyb-config.module';
-import { LOEYBConfigService } from 'configs/loeyb-config.service';
+import { LOEYBConfigModule } from '../../../libs/common/src/config/loeyb-config.module';
+import { LOEYBConfigService } from '../../../libs/common/src/config/loeyb-config.service';
 import { AUTHENTICATION_FACTORY } from 'apps/gateway/src/factory';
 
 @Module({
