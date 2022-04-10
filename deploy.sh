d732b145f8a6f7ced5b6170eadde27322fef534d
IbrohimIsroilov <tursunali777@mail.ru>
2022-04-11 00:42:50 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-STAGE=${1}
-IP_LIST=$(aws ec2 describe-instances --filters --query "Reservations[].Instances[].[PublicIpAddress,Tags[?Key=='Name'].Value[]]" --output text --region ap-northeast-2 | sed '$!N;s/\n/\t/' | grep loeyb-${STAGE}-api | awk '{print $1}')
-
-for IP in ${IP_LIST};
 do
     echo '===================='
-    echo ${IP}
-    ssh  -o "StrictHostKeyChecking no" -i loeyb-api.pem ec2-user@${IP} /home/ec2-user/docker.sh
+    ssh  -o "StrictHostKeyChecking no" -i loeyb-api.pem ec2-user@3.39.85.24 /home/ec2-user/docker.sh
     sleep 3
 done;
