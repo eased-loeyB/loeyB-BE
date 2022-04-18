b0f701c0f2ee1b9ea1faeee1aad806381ddb05b5
IbrohimIsroilov <tursunali777@mail.ru>
2022-04-19 04:50:13 +0900

    test deployment 2

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 
 echo '=========================================='
-ssh  -o "StrictHostKeyChecking no" -i loeyb-api.pem ubuntu@$3.38.117.249 /home/ubuntu/docker.sh
+ssh  -o "StrictHostKeyChecking no" -i loeyb-api.pem ubuntu@3.38.117.249 /home/ubuntu/docker.sh
 echo '=========================================='
