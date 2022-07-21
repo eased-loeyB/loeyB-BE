109c9d6db43c147486328a813b4e1b9fb17986f6
IsroilovIbrohim <ibrohim@capa.ai>
2022-07-21 21:40:26 +0900

    update

--- libs/database/src/ormconfig.ts
+++ libs/database/src/ormconfig.ts
@@ -26,7 +26,7 @@ async function ormConfig(): Promise<TypeOrmModuleOptions> {
     database: config.dbDatabase,
     schema: config.dbSchema,
     keepConnectionAlive: true,
-    entities: [join(__dirname, '/entities/*.entity.{ts,js}')],
+    entities: [],
     migrations: [join(__dirname, 'migrations/v1/*.{ts, js}')],
     subscribers: [],
     synchronize: config.dbSync,
